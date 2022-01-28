e45466b8eb997d50bee51346ac80378ba0b76992
Onur Baskirt <swtestacademy@gmail.com>
2022-01-29 02:02:29 +0400

    SpringBootTest class is added. Not mandatory update.

--- src/test/java/com/swtestacademy/springbootselenium/annotations/SeleniumTest.java
+++ src/test/java/com/swtestacademy/springbootselenium/annotations/SeleniumTest.java
@@ -1,5 +1,6 @@
 package com.swtestacademy.springbootselenium.annotations;
 
+import com.swtestacademy.springbootselenium.SpringSeleniumApplication;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +12,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(SpringExtension.class)
-@SpringBootTest()
+@SpringBootTest(classes = SpringSeleniumApplication.class)
 public @interface SeleniumTest {
 }
