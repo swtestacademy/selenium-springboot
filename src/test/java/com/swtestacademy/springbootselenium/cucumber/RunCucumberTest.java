1d1b067697dd600bb5c80e3863a3673e04f2cdb8
Onur Baskirt <swtestacademy@gmail.com>
2022-02-27 13:54:09 +0400

    Cucumber related updates has been implemented.

--- src/test/java/com/swtestacademy/springbootselenium/cucumber/RunCucumberTest.java
+++ src/test/java/com/swtestacademy/springbootselenium/cucumber/RunCucumberTest.java
@@ -5,14 +5,14 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com/swtestacademy/springbootselenium/cucumber/features")
+@SelectDirectories("src/test/java/com/swtestacademy/springbootselenium/cucumber/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.swtestacademy.springbootselenium.cucumber")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@google or @author")
+//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@negative")
 public class RunCucumberTest {
 }
